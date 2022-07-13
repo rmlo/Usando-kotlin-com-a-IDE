55f2303530175ce2ee5b94303ba28a828e4c716c
rmlo <rmlomoreira@gmail.com>
2022-07-13 16:38:30 -0300

    Criando a classe conta

---
+++ src/main/kotlin/rmlo/bank/Conta.kt
@@ -0,0 +1,18 @@
+package rmlo.bank
+
+import java.math.BigDecimal
+
+class Conta(
+    val agencia : String,
+    val numero : String,
+    val saldo : BigDecimal
+    )
+
+{
+    fun deposito(valor: BigDecimal) {
+
+    }
+    fun saque(valor: BigDecimal){
+
+    }
+}
